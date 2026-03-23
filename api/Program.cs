05199e0d9b85f8cb28c2c658a5d6f3142218cac3
Fuad123yuriygie <farukfuadhassan@gmail.com>
2026-03-24 00:42:34 +0600

    Add project files.

---
+++ api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Add a simple root endpoint so navigating to the server root doesn't return 404
+app.MapGet("/", () => "API is running");
+
+app.Run();
