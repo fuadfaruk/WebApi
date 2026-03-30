eaeb0a0ba2d1ceddde9851b3a88330e4e71e9e4e
fuadfaruk <fuadfaruk707@gmail.com>
2026-03-30 09:46:20 +0600

    Added DELETE For Comments

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -2,11 +2,11 @@
 {
     public class Comment
     {
-        public int Id { get; set; } // Foreign key property
+        public int Id { get; set; } 
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
-        public int? StockId { get; set; }
+        public int? StockId { get; set; } // Foreign key property
         public Stock? Stock { get; set; } // Navigation property
     }
 }
